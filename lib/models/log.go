a6cd4522d84111449f0bdefaeda5a93948d0ce13
Bhagi Moola <bhagimoola1979@gmail.com>
2022-12-19 21:08:10 -0500

    refactoring 2

--- lib/models/log.go
+++ lib/models/log.go
@@ -10,7 +10,9 @@ import (
 	"github.com/swethabhageerath/logging/lib/constants"
 )
 
-type Log struct {
+type Options func(*log)
+
+type log struct {
 	Frames    string `json:"Frames"`
 	AppName   string `json:"AppName"`
 	User      string `json:"User"`
@@ -23,7 +25,13 @@ type Log struct {
 	Observers []io.Writer `json:"_"`
 }
 
-func (l *Log) Attach(observer io.Writer) (bool, error) {
+type Frame struct {
+	FilePath   string
+	Operation  string
+	LineNumber int
+}
+
+func (l *log) Attach(observer io.Writer) (bool, error) {
 	for _, o := range l.Observers {
 		if o == observer {
 			return false, errors.New("logging observer already attached")
@@ -34,7 +42,7 @@ func (l *Log) Attach(observer io.Writer) (bool, error) {
 	return true, nil
 }
 
-func (l *Log) Detach(observer io.Writer) (bool, error) {
+func (l *log) Detach(observer io.Writer) (bool, error) {
 	for i, o := range l.Observers {
 		if o == observer {
 			l.Observers = append(l.Observers[:i], l.Observers[i+1:]...)
@@ -44,7 +52,7 @@ func (l *Log) Detach(observer io.Writer) (bool, error) {
 	return false, errors.New("logging observer not attached")
 }
 
-func (l *Log) Notify() (bool, error) {
+func (l *log) Notify() (bool, error) {
 	for _, o := range l.Observers {
 		_, err := o.Write([]byte(l.String()))
 		if err != nil {
@@ -54,76 +62,72 @@ func (l *Log) Notify() (bool, error) {
 	return true, nil
 }
 
-func (l *Log) AddAppName(appName string) *Log {
-	if appName == "" {
-		panic("appname is mandatory")
+func WithMandatoryFields(appName string, user string, loglevel constants.LogLevel) Options {
+	return func(l *log) {
+		if appName == "" {
+			panic("appname is mandatory")
+		}
+
+		if user == "" {
+			panic("user is mandatory")
+		}
+
+		l.LogLevel = loglevel.String()
+		l.AppName = appName
+		l.User = user
 	}
-	l.AppName = appName
-	return l
 }
 
-func (l *Log) AddUser(user string) *Log {
-	if user == "" {
-		panic("user is mandatory")
-	}
-	l.User = user
-	return l
-}
-
-func (l *Log) AddLogLevel(logLevel constants.LogLevel) *Log {
-	l.LogLevel = logLevel.String()
-	return l
-}
-
-func (l *Log) AddRequestId(requestId string) *Log {
-	if requestId == "" {
-		panic("requestid is mandatory")
-	}
-	l.RequestId = requestId
-	return l
-}
-
-func (l *Log) AddDetails(details string) *Log {
-	l.Details = details
-	return l
-}
-
-func (l *Log) AddParams(params string) *Log {
-	l.Params = params
-	return l
-}
-
-func (l *Log) AddStackTrace(e error) *Log {
-	type stackTracer interface {
-		StackTrace() errors.StackTrace
-	}
-
-	var stackBuilder strings.Builder
-
-	if err, ok := e.(stackTracer); ok {
-		for _, f := range err.StackTrace() {
-			stack := fmt.Sprintf("%+v ", f)
-
-			if !strings.Contains(stack, "/runtime") {
-				stackBuilder.WriteString(strings.Replace(stack, "\n\t", " ", -1))
-			}
+func WithRequestId(requestId string) Options {
+	return func(l *log) {
+		if requestId == "" {
+			l.RequestId = requestId
 		}
 	}
-
-	l.Frames = stackBuilder.String()
-	return l
 }
 
-func (l *Log) String() string {
+func WithDetails(details string) Options {
+	return func(l *log) {
+		if details == "" {
+			l.Details = details
+		}
+	}
+}
+
+func WithParams(params string) Options {
+	return func(l *log) {
+		if params == "" {
+			l.Params = params
+		}
+	}
+}
+
+func WithStackTrace(err error) Options {
+	return func(l *log) {
+		type stackTracer interface {
+			StackTrace() errors.StackTrace
+		}
+
+		var stackBuilder strings.Builder
+
+		if err, ok := err.(stackTracer); ok {
+			for _, f := range err.StackTrace() {
+				stack := fmt.Sprintf("%+v ", f)
+
+				if !strings.Contains(stack, "/runtime") {
+					stackBuilder.WriteString(strings.Replace(stack, "\n\t", " ", -1))
+				}
+			}
+		}
+
+		l.Frames = stackBuilder.String()
+	}
+}
+
+func (l *log) String() string {
 	j, err := json.Marshal(l)
 	if err != nil {
 		panic(err)
 	}
 	return string(j)
-}
-
-type Frame struct {
-	FilePath   string
-	Operation  string
-	LineNumber int
 }
